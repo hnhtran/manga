b7634e000b6dc17ef8d42ef6b112516329dcc7e5
hnhtran <tran_eetas@yahoo.com>
2022-09-09 13:21:14 +1000

    model category edit

--- MangaWebApp/Models/Category.cs
+++ MangaWebApp/Models/Category.cs
@@ -1,6 +1,14 @@
-﻿namespace MangaWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangaWebApp.Models
 {
     public class Category
     {
+        []
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
