09130556d796e10ff8f4de28a907ace1c26888f3
hnhtran <tran_eetas@yahoo.com>
2022-09-09 17:35:54 +1000

    Ctegory ctrl edit

--- MangaWebApp/Controllers/CategoryController.cs
+++ MangaWebApp/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MangaWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MangaWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var objCatList = _db.CategoryTable.ToList();
             return View();
         }
     }
